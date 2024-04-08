7582ccb0fd82d23046e0a12d6beb526c7e14e393
ZeusJunior <32263615+ZeusJunior@users.noreply.github.com>
2024-04-08 21:55:31 +0200

    Update backpacktf-extra-link.user.js

--- backpacktf-extra-link.user.js
+++ backpacktf-extra-link.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Backpack.tf Extra Link
 // @namespace    https://steamcommunity.com/profiles/76561198144346135
-// @version      1.1.11
+// @version      1.2.0
 // @description  Add a link on bptf to the stn page of that item
 // @author       Zeus_Junior
 // @match        https://backpack.tf/stats/*
@@ -26,19 +26,15 @@
         // nothing
     } else if (nameLocation.includes('Festivized%20')) {
         itemLinkSTN = pathArray[2] + ' ' + nameLocation.substr(13);
-        // $( ".price-boxes" ).append('<a class="price-box" href="item.com" data-tip="top" target="_blank"  title="Scrap.tf"> <img src="https://scrap.tf/favicon.ico"> <div class="text"> <div class="value"> $x </div> <div class="subtitle"> x </div> </div> </a>');
-        $( ".price-boxes" ).append('<a class="price-box" href="https://stntrading.eu/item/tf2/' + itemLinkSTN + '" data-tip="top" target="_blank"  title="Stntrading.eu"> <img src="https://stntrading.eu/assets/img/favicons/favicon.ico"> <div class="text"> <div class="value"> Click me </div> <div class="subtitle"> Pls click </div> </div> </a>');
-    } else if (qualityLocation === 'Unusual'){
-        itemLinkSTN = pathArray[2] + ' ' + nameLocation + '&isUnusual=1';
-        // $( ".price-boxes" ).append('<a class="price-box" href="item.com" data-tip="top" target="_blank"  title="Scrap.tf"> <img src="https://scrap.tf/favicon.ico"> <div class="text"> <div class="value"> $x </div> <div class="subtitle"> x </div> </div> </a>');
-        $( ".price-boxes" ).append('<a class="price-box" href="https://stntrading.eu/search?query=' + itemLinkSTN + '" data-tip="top" target="_blank"  title="Stntrading.eu"> <img src="https://stntrading.eu/assets/img/favicons/favicon.ico"> <div class="text"> <div class="value"> Click me </div> <div class="subtitle"> Pls click </div> </div> </a>');
+        $( ".price-boxes" ).append('<a class="price-box" href="https://stntrading.eu/item/tf2/' + itemLinkSTN + '" target="_blank""> <img src="https://stntrading.eu/assets/img/logos/icon.svg"> <div class="text"> <div class="value"> STN </div> </div> </a>');
+    } else if (qualityLocation === 'Unusual') {
+        itemLinkSTN = pathArray[2] + ' ' + $('.stats-header-title').text().trim();
+        $( ".price-boxes" ).append('<a class="price-box" href="https://stntrading.eu/item/tf2/' + itemLinkSTN + '" target="_blank""> <img src="https://stntrading.eu/assets/img/logos/icon.svg"> <div class="text"> <div class="value"> STN </div> </div> </a>');
     } else if (qualityLocation != 'Unique') {
         itemLinkSTN = pathArray[2] + ' ' + nameLocation;
-        // $( ".price-boxes" ).append('<a class="price-box" href="item.com" data-tip="top" target="_blank"  title="Scrap.tf"> <img src="https://scrap.tf/favicon.ico"> <div class="text"> <div class="value"> $x </div> <div class="subtitle"> x </div> </div> </a>');
-        $( ".price-boxes" ).append('<a class="price-box" href="https://stntrading.eu/item/tf2/' + itemLinkSTN + '" data-tip="top" target="_blank"  title="Stntrading.eu"> <img src="https://stntrading.eu/assets/img/favicons/favicon.ico"> <div class="text"> <div class="value"> Click me </div> <div class="subtitle"> Pls click </div> </div> </a>');
+        $( ".price-boxes" ).append('<a class="price-box" href="https://stntrading.eu/item/tf2/' + itemLinkSTN + '" target="_blank""> <img src="https://stntrading.eu/assets/img/logos/icon.svg"> <div class="text"> <div class="value"> STN </div> </div> </a>');
     } else {
         itemLinkSTN = nameLocation;
-        // $( ".price-boxes" ).append('<a class="price-box" href="item.com" data-tip="top" target="_blank"  title="Scrap.tf"> <img src="https://scrap.tf/favicon.ico"> <div class="text"> <div class="value"> $x </div> <div class="subtitle"> x </div> </div> </a>');
-        $( ".price-boxes" ).append('<a class="price-box" href="https://stntrading.eu/item/tf2/' + itemLinkSTN + '" data-tip="top" target="_blank"  title="Stntrading.eu"> <img src="https://stntrading.eu/assets/img/favicons/favicon.ico"> <div class="text"> <div class="value"> Click me </div> <div class="subtitle"> Pls click </div> </div> </a>');
+        $( ".price-boxes" ).append('<a class="price-box" href="https://stntrading.eu/item/tf2/' + itemLinkSTN + '" target="_blank""> <img src="https://stntrading.eu/assets/img/logos/icon.svg"> <div class="text"> <div class="value"> STN </div> </div> </a>');
     }
 })();
